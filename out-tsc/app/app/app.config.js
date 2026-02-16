bc216b69b4387d70abc00125f3f7f4f5d85ea4d7
lukeireland1972 <luke@creventa.com>
2026-02-16 09:08:06 +0000

    Workspace Cleanup

--- out-tsc/app/app/app.config.js
+++
@@ -1,8 +0,0 @@
-import { provideHttpClient, withInterceptors } from '@angular/common/http';
-import { provideRouter } from '@angular/router';
-import { routes } from './app.routes';
-import { authInterceptor } from './services/auth.interceptor';
-export const appConfig = {
-    providers: [provideRouter(routes), provideHttpClient(withInterceptors([authInterceptor]))]
-};
-//# sourceMappingURL=app.config.js.map
